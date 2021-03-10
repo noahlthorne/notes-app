0c3e4981f8c48dad08075370c7faf6fdc6564ca1
Noah Thorne <48963204+noahlthorne@users.noreply.github.com>
2021-03-10 21:32:22 +0900

    formatted app

--- app.js
+++ app.js
@@ -6,54 +6,54 @@ const fetch = require("node-fetch");
 yargs.version("1.1.0");
 
 yargs.command({
-  command: "add",
-  describe: "Adds a new note",
-  builder: {
-    title: {
-      describe: "Title",
-      demandOption: true,
-      type: "string",
+    command: "add",
+    describe: "Adds a new note",
+    builder: {
+        title: {
+            describe: "Title",
+            demandOption: true,
+            type: "string",
+        },
+        body: {
+            describe: "Note body",
+            demandOption: true,
+            type: "string",
+        },
     },
-    body: {
-      describe: "Note body",
-      demandOption: true,
-      type: "string",
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body);
     },
-  },
-  handler: function (argv) {
-    notes.addNote(argv.title, argv.body);
-  },
 });
 
 yargs.command({
-  command: "remove",
-  describe: "Removes a note",
-  builder: {
-    title: {
-      describe: "Note title",
-      demandOption: true,
-      type: "string",
+    command: "remove",
+    describe: "Removes a note",
+    builder: {
+        title: {
+            describe: "remove a note",
+            demandOption: true,
+            type: "string",
+        },
     },
-  },
-  handler: function (argv) {
-    notes.remmoveNote(argv.title);
-  },
+    handler: function (argv) {
+        notes.remmoveNote(argv.title);
+    },
 });
 
 yargs.command({
-  command: "list",
-  describe: "Lists notes",
-  handler: function () {
-    console.log("listing notes...");
-  },
+    command: "list",
+    describe: "Lists notes",
+    handler: function () {
+        console.log("listing notes...");
+    },
 });
 
 yargs.command({
-  command: "read",
-  describe: "displays the note",
-  handler: function () {
-    console.log("reading notes....");
-  },
+    command: "read",
+    describe: "displays the note",
+    handler: function () {
+        console.log("reading notes....");
+    },
 });
 
 yargs.parse();
