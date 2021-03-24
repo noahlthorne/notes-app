1b0e5a1a6e7a78b0e5bf24698198c6f9bb491a93
Noah Thorne <noahlthorne@gmail.com>
2021-03-24 22:53:49 +0900

    update error message when saving notes

--- notes.js
+++ notes.js
@@ -1,50 +1,50 @@
 const fs = require("fs");
 
 const getNotes = () => {
-  return "your notes...";
+	return "your notes...";
 };
 
 const addNote = (title, body) => {
-  const notes = loadNotes();
-  const duplicateNotes = notes.filter((note) => {
-    return note.title === title;
-  });
-  if (duplicateNotes.length === 0) {
-    notes.push({
-      title: title,
-      body: body,
-    });
-    saveNotes(notes);
-    console.log("note updated");
-  } else {
-    console.error("note title taken");
-  }
+	const notes = loadNotes();
+	const duplicateNotes = notes.filter((note) => {
+		return note.title === title;
+	});
+	if (duplicateNotes.length === 0) {
+		notes.push({
+			title: title,
+			body: body,
+		});
+		saveNotes(notes);
+		console.log("note updated");
+	} else {
+		console.error("note title already taken");
+	}
 };
 
 const remmoveNote = (title) => {
-  const notes = loadNotes();
-  const noteToRemove = notes.find((note) => note.title === title);
-  notes.pop(noteToRemove);
-  saveNotes(notes);
+	const notes = loadNotes();
+	const noteToRemove = notes.find((note) => note.title === title);
+	notes.pop(noteToRemove);
+	saveNotes(notes);
 };
 
 const saveNotes = (notes) => {
-  const dataJSON = JSON.stringify(notes);
-  fs.writeFileSync("notes.json", dataJSON);
+	const dataJSON = JSON.stringify(notes);
+	fs.writeFileSync("notes.json", dataJSON);
 };
 
 const loadNotes = () => {
-  try {
-    const dataBuffer = fs.readFileSync("notes.json");
-    const dataJSON = dataBuffer.toString();
-    return JSON.parse(dataJSON);
-  } catch (e) {
-    return [];
-  }
+	try {
+		const dataBuffer = fs.readFileSync("notes.json");
+		const dataJSON = dataBuffer.toString();
+		return JSON.parse(dataJSON);
+	} catch (e) {
+		return [];
+	}
 };
 
 module.exports = {
-  getNotes: getNotes,
-  addNote: addNote,
-  remmoveNote: remmoveNote,
+	getNotes: getNotes,
+	addNote: addNote,
+	remmoveNote: remmoveNote,
 };
